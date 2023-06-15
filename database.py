9a5a0463bc6ed12f8c97b8445d2d57dae1f842d7
Yoshio <yoshiokiyamusu@gmail.com>
2023-06-14 21:51:38 -0400

    -

--- database.py
+++ database.py
@@ -3,5 +3,5 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 
-#comment 1 #creado en branch master
+#comment 1 #creado en branch master, comentario random despues de comentario hecho desde la web github
 
